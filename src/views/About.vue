e3d05d5aa3f6859443fee58f2318ab744076e8a0
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2020-05-14 21:55:56 +0100

    added navbar to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <c-box px="3rem" py="2rem" bg="#0d8e541a">
+    <Navbar />
+  </c-box>
 </template>
+
+<script>
+import { CBox } from "@chakra-ui/vue";
+import Navbar from "@/components/Navbar";
+export default {
+  name: "about",
+  components: {
+    CBox,
+    Navbar
+  }
+};
+</script>
