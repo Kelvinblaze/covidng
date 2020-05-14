355562b16b4cbefe7b0cb534b925649feb4d3be8
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2020-05-14 22:00:22 +0100

    minor changes on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,23 @@
 <template>
-  <c-box px="3rem" py="2rem" bg="#0d8e541a">
-    <Navbar />
+  <c-box>
+    <c-box px="3rem" py="2rem" bg="#0d8e541a" h="50vh">
+      <Navbar />
+
+      <c-box>
+        <c-text fontSize="6xl" fontWeight="bold" py="2rem">About</c-text>
+      </c-box>
+    </c-box>
   </c-box>
 </template>
 
 <script>
-import { CBox } from "@chakra-ui/vue";
+import { CBox, CText } from "@chakra-ui/vue";
 import Navbar from "@/components/Navbar";
 export default {
   name: "about",
   components: {
     CBox,
+    CText,
     Navbar
   }
 };
